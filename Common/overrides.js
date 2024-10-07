d07d23e9db160a5ea7dd9fbe7b93bc5b31c631c9
digi-ron <32566377+digi-ron@users.noreply.github.com>
2024-10-07 20:25:07 +1100

    Delete overrides.js

--- Common/overrides.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const blockquotes = document.querySelectorAll("blockquote");
-    blockquotes.forEach(blockquote => {
-        if (blockquote.textContent.trim().startsWith("NOTE:")) {
-            blockquote.style.color = "darkgoldenrod"; // Change to your desired color
-        }
-    });
-});
