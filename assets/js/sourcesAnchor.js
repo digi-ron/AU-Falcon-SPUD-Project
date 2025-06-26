26ba7f1ba446ed06fb41e4ffa43637dd1918bc3a
digi-ron <32566377+digi-ron@users.noreply.github.com>
2025-06-27 01:23:37 +1000

    Create sourcesAnchor.js

---
+++ assets/js/sourcesAnchor.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a[href$="Credits.html#sources"]');
+    links.forEach(link => {
+        const currenthref = link.getAttribute('href');
+        const text = link.textContent.trim();
+        const newHref = currenthref + `#:~:text=${encodeURIComponent(text)}`;
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.location = newHref;
+        });
+    })
+});
+
